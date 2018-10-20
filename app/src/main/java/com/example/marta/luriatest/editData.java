b9c87d3550d47d40d07ac4ba859ddf781b4840cb
MartaDziemba <34282722+MartaDziemba@users.noreply.github.com>
2018-10-20 15:11:30 +0200

    Created method saveData.

--- app/src/main/java/com/example/marta/luriatest/editData.java
+++ app/src/main/java/com/example/marta/luriatest/editData.java
@@ -2,9 +2,17 @@ package com.example.marta.luriatest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import butterknife.ButterKnife;
@@ -12,8 +20,11 @@ import butterknife.OnClick;
 
 public class editData extends AppCompatActivity {
 
+    EditText editTextFirstName, editTextLastName, editTextAge, editTextIDNumber, editTextDescription;
+
     @OnClick(R.id.buttonTest)
     void OnClickEditData(){
+
         Intent intent = new Intent(editData.this, moveToFingerPath.class);
         startActivity(intent);
     }
@@ -23,6 +34,45 @@ public class editData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editdata);
         ButterKnife.bind(this);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextIDNumber = findViewById(R.id.editTextIDNumber);
+        editTextDescription = findViewById(R.id.editTextDescription);
     }
 
+    public void saveData(View v){
+        Long date = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+        String fileName = sdf.format(date) + ".csv";
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),fileName);
+
+        if(file.exists() && !file.isDirectory()){
+
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(editTextFirstName.getText().toString().getBytes());
+                fos.write('\n');
+                fos.write(editTextLastName.getText().toString().getBytes());
+                fos.write('\n');
+                fos.write(editTextAge.getText().toString().getBytes());
+                fos.write('\n');
+                fos.write(editTextIDNumber.getText().toString().getBytes());
+                fos.write('\n');
+                fos.write(editTextDescription.getText().toString().getBytes());
+                fos.close();
+
+                Toast.makeText(this, "File saved!", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            Toast.makeText(this, "File not saved!", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
