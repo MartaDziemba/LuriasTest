cb8fa64e703027c49db10e23dd288a01d2a6d9d6
MartaDziemba <34282722+MartaDziemba@users.noreply.github.com>
2018-11-11 22:02:57 +0100

    Hiding keyboard in edit_existing_data.

--- app/src/main/java/com/example/marta/luriatest/editExistingData.java
+++ app/src/main/java/com/example/marta/luriatest/editExistingData.java
@@ -3,7 +3,10 @@ package com.example.marta.luriatest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -16,5 +19,14 @@ public class editExistingData extends AppCompatActivity {
         setContentView(R.layout.edit_existing_data);
         ButterKnife.bind(this);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.allRelativeLayout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
     }
 }
